10f27719d8bf99863e3fcc2ec4d88f6f5f66b489
Harish <hhsudhan53@gmail.com>
2025-06-14 08:07:05 +0530

    debug process for passwordgenerator

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,8 +1,8 @@
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 
 
  const PasswordGenerator = () => {
-                const [length,setLength]=useState(8)
+                const [length,setLength]=useState(10)
                 const [numberAllowed,setNumberAllowed]=useState(false)
                 const[symbolAllowed,setSymbolAllowed]=useState(false)
                 const[password,setPassword]=useState('')
@@ -28,35 +28,47 @@ import { useCallback, useEffect, useState } from "react"
 
 
 
+                    const inputRef=useRef()
+
+
+
+                    const copyClipboard=()=>{
+                        window.navigator.clipboard.writeText(password)                       
+                    }
+
 
 
                 useEffect(()=>{
                     generatorPassword()
-                },[length,password,numberAllowed,symbolAllowed])
+                },[length,numberAllowed,symbolAllowed])
 
 
 
   return (
-                <div className='bg-black w-full h-screen flex justify-center '>
+                <div className='bg-black w-full h-screen flex justify-center  container'>
                     <div className=' flex flex-col w-1/3 h-[100px] bg-gray-600 mt-10 rounded-md'>
                                 <div className="flex justify-center text-white ">
                                     <h1>Password generator</h1>
                                 </div>
-                                <div className="flex  justify-center">
+                                <div className="flex  justify-center flex-wrap ">
                                                 <input
                                                  type="text"
-                                                 value={password} className="w-4/5 h-5 text-amber-700" readOnly  placeholder="password"/>
+                                                 value={password} className="w-3/5 h-5 text-amber-700 rounded-sm" readOnly  placeholder="password"/>
 
                                                 <div className="flex justify-center align-middle mb-5">
-                                                     <button className="bg-blue-600 text-white rounded-sm h-5">Copy</button>
+                                                     <button className="bg-blue-600 text-white  h-5 rounded-sm text-sm outline-none" ref={inputRef} onClick={copyClipboard}>copy</button>
                                                 </div>
                                            
                                             
                                 </div>
                                 <div className="flex justify-center w-full gap-2 ">
-                                    <input className="text-blue-600" type="range" value={length} min={8} max={50} onChange={(e)=> setLength(e.target.value)}/> <label htmlFor="length" className="text-orange-400" >Length:<span className="text-black">{length}</span></label>
-                                    <input type="checkbox" defaultChecked={numberAllowed} onChange={()=>{setNumberAllowed((prev)=>!prev)}} /><lable>Number</lable>
-                                    <input type="checkbox"  defaultChecked={symbolAllowed} onChange={()=>setSymbolAllowed((prev)=>!prev)}/><lable>Character</lable>
+                                    <input className="text-blue-600" type="range" value={length} min={10} max={50} onChange={e=>{ setLength(Number(e.target.value))
+                                       
+                                    }
+                                
+                                    }/> <label htmlFor="length" className="text-orange-400" >Length:{length}</label>
+                                    <input type="checkbox" defaultChecked={numberAllowed} onChange={()=>{setNumberAllowed((prev)=>!prev)}} /><lable  className="text-orange-400">Number</lable>
+                                    <input type="checkbox"  defaultChecked={symbolAllowed} onChange={()=>setSymbolAllowed((prev)=>!prev)}/><lable className="text-orange-400">Symbol</lable>
 
                                 </div>
 
